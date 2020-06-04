bf2fddf0750c34fc814322fc86664ebbe603b6eb
ICanHazCode <X_Blaster@panhandle.rr.com>
2020-06-04 16:53:07 -0500

    fixed formatting

--- Patches.cs
+++ Patches.cs
@@ -10,60 +10,64 @@ using HarmonyLib;
 namespace Vendomatic
 {
 
-	[HarmonyPatch(typeof(VendingMachine), "OnCustomImportFinished")]
-	class VendingMachineOnCustomImportFinishedPatch
-	{
-		static bool Prefix(VendingMachine __instance)
-		{
-			VendingMachineOnCustomImportFinishedPatch.PatchedOnCustomImportFinished(__instance);
-			return false;
-		}
+    [HarmonyPatch(typeof(VendingMachine), "OnCustomImportFinished")]
+    class VendingMachineOnCustomImportFinishedPatch
+    {
+        static bool Prefix(VendingMachine __instance)
+        {
+            VendingMachineOnCustomImportFinishedPatch.PatchedOnCustomImportFinished(__instance);
+            return false;
+        }
 
-		static void PatchedOnCustomImportFinished(VendingMachine __instance)
-		{
-			// replaces base.OnCustomImportFinished()
-			__instance.FinishedImporting = true;
+        static void PatchedOnCustomImportFinished(VendingMachine __instance)
+        {
+            // replaces base.OnCustomImportFinished(), since we can't call a base class from outside
+            __instance.FinishedImporting = true;
 
-			if (GameManager.IsServer
-				&& __instance.ImportingThing is Stackable importingStackable)//this also does a null check
-			{
-				foreach (Slot slot in __instance.Slots)
-				{
-					//find an internal slot with the same item type that has room
-					if (!slot.IsInteractable
-						&& slot.Occupant is Stackable occupant
-						&& occupant.PrefabHash == importingStackable.PrefabHash
-						&& occupant.MaxQuantity > occupant.NetworkQuantity)
-					{
-						OnServer.Merge(occupant, importingStackable);
-					}
-					else
-					{
-						continue;
-					}
-					// We're done merging
-					if (importingStackable.NetworkQuantity == 0) break;
-				}
-				// Skip adding the now-empty item if we are empty.
-				// The empty stack was already deleted when its quantity was set to zero.
-				if (importingStackable.NetworkQuantity > 0)
-				{
-					foreach (Slot slot in __instance.Slots)
-					{
-						if (!slot.IsInteractable && slot.Occupant == null)
-						{
-							OnServer.MoveToSlot(importingStackable, slot);
-							break;
-						}
-					}
-				}
+            if (!GameManager.IsServer)
+                return;
 
-				OnServer.Interact(__instance.InteractImport, 0, false);
-				if (GameManager.GameState == GameState.Running && __instance.CurrentSlot.Occupant == null)
-				{
-					__instance.PlanForward();
-				}
-			}
-		}
-	}
+            //pattern matching also does a null check
+            if (__instance.ImportingThing is Stackable importingStackable)
+            {
+                foreach (Slot slot in __instance.Slots)
+                {
+                    //find an internal slot with the same item type that has room
+                    if (!slot.IsInteractable
+                        && slot.Occupant is Stackable occupant
+                        && occupant.PrefabHash == importingStackable.PrefabHash
+                        && occupant.MaxQuantity > occupant.NetworkQuantity)
+                    {
+                        OnServer.Merge(occupant, importingStackable);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                    //fast exit if the import is now empty
+                    if (importingStackable.NetworkQuantity == 0) break;
+                }
+
+                // It is possible for us to not be empty, in which case a new slot should be started.
+                if (importingStackable.NetworkQuantity > 0)
+                {
+                    foreach (Slot slot in __instance.Slots)
+                    {
+                        if (!slot.IsInteractable && !slot.Occupant)
+                        {
+                            OnServer.MoveToSlot(__instance.ImportingThing, slot);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            OnServer.Interact(__instance.InteractImport, 0, false);
+
+            if (GameManager.GameState == GameState.Running && !__instance.CurrentSlot.Occupant)
+            {
+                __instance.PlanForward();
+            }
+        }
+    }
 }
